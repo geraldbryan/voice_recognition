1da2c73408d0512ffca7074e01af77393ed906a3
geraldbryan <geraldbryan9914@gmail.com>
2022-09-26 15:18:40 +0700

    Initial Commit

---
+++ voice_recognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  voice_recognition
+//
+//  Created by Gerald Bryan on 26/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
